fb1b27f2b4e1b970eb834668313491049ae92bc0
Abdul-Hafiz Aderemi <abdulhafizaderemi@gmail.com>
2024-11-29 22:05:44 +0100

    Update comment.model.js

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
-const CommentSchema = new Mongoose.Schema({})
+const CommentSchema = new Mongoose.Schema({
+name: {
+type: mongoose.Schema
+Types.ObjectId,
+ref: 'User'
+required: true 
+},
+comment: {
+type: String,
+required: true
+},
+}, {timestamps: true})
 
 export default mongoose.model('Comment' CommentSchema)
