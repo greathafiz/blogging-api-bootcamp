1646d67e48fa8e80bef4230cd612663a7791237c
Abdul-Hafiz Aderemi <abdulhafizaderemi@gmail.com>
2024-11-30 20:54:39 +0100

    comment controller

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,14 @@
+import Comment from '../models/comment.model.js'
+
+export const createComment = (req, res) => {
+res.send('create comment')
+}
+
+export const getAllComments = (req, res) => {
+res.send('get all comments')
+}
+
+export const deleteComment = (req, res) => {
+res.send('delete comment')
+}
+
